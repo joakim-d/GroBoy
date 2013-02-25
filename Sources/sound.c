583eee496d19f6ad2f6a294fbdc54c7c2d02f20f
burger <burger@burgerfactory.(none)>
2013-02-25 11:35:41 +0100

    Programmation APU

--- Sources/sound.c
+++ Sources/sound.c
@@ -1,1 +1,133 @@
+#include "sound.h"
+
+apu_t apu;
+
+void init(){
+	apu.channel1.sweep_period = 0x00;
+	apu.channel1.sweep_shift = 0x00;
+	apu.channel1.sweep_regulation = 0; 
+	apu.channel1.wave_duty = 0x00;
+	apu.channel1.sound_length = 0x00;
+	apu.channel1.initial_volume = 0x00;
+	apu.channel1.nb_sweep_env = 0x00;
+	apu.channel1.env_direction = 0; 
+	apu.channel1.freq_low = 0x00;
+	apu.channel1.freq_high = 0x00;
+	apu.channel1.initier = 0; 
+	apu.channel1.counter_consec = 0; 
+
+	apu.channel2.sound_length = 0x00;
+	apu.channel2.wave_duty = 0x00;
+	apu.channel2.initial_env_volume = 0x00;
+	apu.channel2.env_direction = 0; 
+	apu.channel2.nb_sweep_env = 0x00;
+	apu.channel2.freq_low = 0x00;
+	apu.channel2.freq_high = 0x00;
+	apu.channel2.initier = 0; 
+	apu.channel2.counter_consec = 0; 
+
+	apu.channel3.sound_trigger = 0; 
+	apu.channel3.sound_length = 0x00;
+	apu.channel3.output_level = 0x00;
+	apu.channel3.freq_low = 0x00;
+	apu.channel3.freq_high = 0x00;
+	apu.channel3.initier = 0; 
+	apu.channel3.counter_consec = 0; 
+
+	apu.channel4.sound_length = 0x00;
+	apu.channel4.initial_env_volume = 0x00;
+	apu.channel4.sweep_number = 0x00;
+	apu.channel4.env_direction = 0; 
+	apu.channel4.freq_shift = 0x00;
+	apu.channel4.freq_division_ratio = 0x00;
+	apu.channel4.step_counter = 0; 
+	apu.channel4.initier = 0; 
+	apu.channel4.counter_consec = 0; 
+
+	apu.sound_controller.so2_output_level = 0x00;
+	apu.sound_controller.so1_output_level = 0x00;
+	apu.sound_controller.output_to_so2 = 0; 
+	apu.sound_controller.output_to_so1 = 0; 
+	apu.sound_controller.all_sounds_trigger = 0; 
+
+	apu.sound_controller.output_sound_so1 = (int *)malloc(3*sizeof(int));
+	apu.sound_controller.output_sound_so2 = (int *)malloc(3*sizeof(int));
+	apu.sound_controller.sound_flags = (int *)malloc(3*sizeof(int));
+	for(int i=0;i<4;i++){
+		apu.sound_controller.output_sound_so1[i] = 0;
+		apu.sound_controller.output_sound_so2[i] = 0;
+		apu.sound_controller.sound_flags[i] = 0;
+	}
+}
+
+void sound_run(){
+	unsigned short address;
+	switch(address){
+		case NR10:
+			if((memory_read(address) & BIT_3) > 0)apu.channel1.sweep_regulation = 1;else apu.channel1.sweep_regulation = 0;
+		break;
+		case NR11:
+		break;
+		case NR12:
+			if((memory_read(address) & BIT_3)>0) apu.channel1.env_direction = 1;else apu.channel1.env_direction = 0;
+		break;
+		case NR13:
+		break;
+		case NR14:
+			if((memory_read(address) & BIT_7)>0) apu.channel1.initier = 1;else apu.channel1.initier = 0;
+			if((memory_read(address) & BIT_6)>0) apu.channel1.counter_consec = 1;else apu.channel1.counter_consec = 0;
+		break;
+		//SC2
+		case NR21:
+		break;
+		case NR22:
+			if((memory_read(address) & BIT_3)>0) apu.channel2.env_direction = 1;else apu.channel2.env_direction = 0;
+		break;
+		case NR23:
+		break;
+		case NR24:
+			if((memory_read(address) & BIT_7)>0) apu.channel2.initier = 1;else apu.channel2.initier = 0;
+			if((memory_read(address) & BIT_6)>0) apu.channel2.counter_consec = 1;else apu.channel2.counter_consec = 0;
+		break;
+		//SC3
+		case NR30:
+			if((memory_read(address) & BIT_7)>0) apu.channel3.sound_trigger = 1;else apu.channel3.sound_trigger = 0;
+		break;
+		case NR31:
+		break;
+		case NR32:
+		break;
+		case NR33:
+		break;
+		case NR34:
+			if((memory_read(address) & BIT_7)>0) apu.channel3.initier = 1;else apu.channel3.initier = 0;
+			if((memory_read(address) & BIT_6)>0) apu.channel3.counter_consec = 1;else apu.channel3.counter_consec = 0;
+		break;
+		//SC4
+		case NR41:
+		break;
+		case NR42:
+			if((memory_read(address) & BIT_3)>0) apu.channel4.env_direction = 1;else apu.channel4.env_direction = 0;
+		break;
+		case NR43:
+			if((memory_read(address) & BIT_3)>0) apu.channel4.step_counter = 1;else apu.channel4.step_counter = 0;
+		break;
+		case NR44:
+			if((memory_read(address) & BIT_7)>0) apu.channel4.initier = 1;else apu.channel4.initier = 0;
+			if((memory_read(address) & BIT_6)>0) apu.channel4.counter_consec = 1;else apu.channel4.counter_consec = 0;
+		break;
+		//SOUND CONTROLL
+		case NR50:
+			if((memory_read(address) & BIT_7)>0) apu.sound_controller.output_to_so2 = 1;else apu.sound_controller.output_to_so2 = 0;
+			if((memory_read(address) & BIT_3)>0) apu.sound_controller.output_to_so1 = 1;else apu.sound_controller.output_to_so1 = 0;
+		break;
+		case NR51:
+		break;
+		case NR52:
+			if((memory_read(address) & BIT_7)>0) apu.sound_controller.all_sounds_trigger = 1;else apu.sound_controller.all_sounds_trigger = 0;
+		break;
+		default:
+		break;
+	}
+}
 
--- Sources/sound.h
+++ Sources/sound.h
@@ -6,76 +6,153 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
-#include "memory.h"
 #include "def.h"
+#include "memory.h"
+
+//Registres du Canal de son 1
+#define NR10 0xff10 //Registre de balayage (R/W)
+#define NR11 0xff11 //Longueur de son et duty d'une forme d'onde (R/W)
+
+#define NR12 0xff12 //Enveloppe de volume (R/W)
+#define NR13 0xff13 //Composantes basses de fréquence (W)
+#define NR14 0xff14 //Composantes hautes de fréquence (R/W)
+
+//Registres du Canal de son 2
+#define NR21 0xff16 //Idem NR11 avec b6 NR24
+#define NR22 0xff17 //Idem NR12
+#define NR23 0xff18 //Idem NR13 
+#define NR24 0xff19 //Idem NR14
+
+//Registres du Canal de son 3
+#define NR30 0xff1a //Son on-off (R/W)
+#define NR31 0xff1b //Durée de son
+#define NR32 0xff1c //Son sortie (R/W)
+#define NR33 0xff1d //Donnée basse de la frequence (W)
+#define NR34 0xff1e //Données hautes de la fréquence (R/W)
+//Les adresses FF30 à FF3F contiennent 32 échantillons 4 bits
+//lus dans le sens 4forts -> 4faibles
+
+//Registres du Canal de son 4
+#define NR41 0xff20 //Durée de son (R/W)
+#define NR42 0xff21 //Enveloppe de volume (R/W)
+#define NR43 0xff22 //Compteur polynomial (R/W)
+#define NR44 0xff23 //Compteur/Consecutif; Initial (R/W)
+
+//Registres de contrôle du son 
+#define NR50 0xff24 //controle canal / on-off / volume (R/W)
+#define NR51 0xff25 //sortie son vers terminal droite / gauche
+#define NR52 0xff26 //son on-off
 
 //Sound Channel 1 : Tone & Sweep
 //Onde carrée
 typedef struct{
-
+	//NR10	
+	BYTE sweep_period;
+	BYTE sweep_shift;
+	int sweep_regulation; // 0 : up, 1 : down
+	//NR11	
+	BYTE wave_duty;
+	BYTE sound_length;
+	//NR12	
+	BYTE initial_volume;
+	BYTE nb_sweep_env;
+	int env_direction; // 0 : down, 1 : up
+	//NR13	
+	BYTE freq_low;
+	//NR14	
+	BYTE freq_high;
+	int initier; // booleen
+	int counter_consec; // booleen
 
 } sc1_t;
 
 //Sound Channel 2 : Idem que SC1 mais sans enveloppe / sweet
 //Onde carrée
 typedef struct{
-
+	//NR21
+	BYTE sound_length;
+	BYTE wave_duty;
+	//NR22
+	BYTE initial_env_volume;
+	int env_direction;
+	BYTE nb_sweep_env;
+	//NR23
+	BYTE freq_low;
+	//NR24
+	BYTE freq_high;
+	int initier; //booleen
+	int counter_consec; //booleen
 
 } sc2_t;
 
 //Sound Channel 3 : Utilisé pour le son digital de sortie et tonalités normales à l'init de l'onde RAM
 //Canal d'échantillons
 typedef struct{
+	//NR30
+	int sound_trigger; //booleen (on/off)
+	//NR31
+	BYTE sound_length;
+	//NR32
+	BYTE output_level;
+	//NR33
+	BYTE freq_low;
+	//NR34
+	BYTE freq_high;
+	int initier; //booleen
+	int counter_consec; //booleen
 
-	
+	/*
+	 * Les registres de FF30 à FF3F 
+	 * stockent des formes d'ondes arbitraires
+	 * déjà présentes dans la cartouche, 
+	 * sous forme de 32 échantillons 4 bits
+	 */
 
 } sc3_t;
 
 //Canal utilisé pour le bruit
 typedef struct{
-	
+	//NR41
+	BYTE sound_length;
+	//NR42
+	BYTE initial_env_volume;
+	BYTE sweep_number;
+	int env_direction; //0 = down, 1 = up
+	//NR43
+	BYTE freq_shift;
+	BYTE freq_division_ratio;
+	int step_counter; // 0 = 15b, 1 = 7b
+	//NR44
+	int initier; //booleen
+	int counter_consec; //booleen
 
 } sc4_t;
 
+//Registres de controle du son
+typedef struct {
+	//NR50
+	BYTE so2_output_level;
+	BYTE so1_output_level;
+	int output_to_so2; //booleen
+	int output_to_so1; //booleen
+	//NR51
+	int* output_sound_so1; //i[0..3], output sound i to so2 terminal
+	int* output_sound_so2; //i[0..3], output sound i to so1 terminal
+	//NR52
+	int all_sounds_trigger; //booleen, 0 : stop all circuits 1 : on
+	int* sound_flags;//i[0..3], flag for sci
+}sound_control_t;
+
 //Audio processing unit
 typedef struct{
-	
-	//Registres du Canal de son 1
-	BYTE NR10;//FF10 Registre de balayage (R/W)
-	BYTE NR11;//FF11 Longueur de son et duty d'une forme d'onde (R/W)
-	BYTE NR12;//FF12 Enveloppe de volume (R/W)
-	BYTE NR13;//FF13 Composantes basses de fréquence (W)
-	BYTE NR14;//FF14 Composantes hautes de fréquence (R/W)
-
-	//Registres du Canal de son 2
-	BYTE NR21;//FF16 Idem NR11 avec b6 NR24
-	BYTE NR22;//FF17 Idem NR12
-	BYTE NR23;//FF18 Idem NR13 
-	BYTE NR24;//FF19 Idem NR14
-
-	//Registres du Canal de son 3
-	BYTE NR30;//FF1A Son on-off (R/W)
-	BYTE NR31;//FF1B Durée de son
-	BYTE NR32;//FF1C Son sortie (R/W)
-	BYTE NR33;//FF1D Donnée basse de la frequence (W)
-	BYTE NR34;//FF1E Données hautes de la fréquence (R/W)
-	//Les adresses FF30 à FF3F contiennent 32 échantillons 4 bits
-	//lus dans le sens 4forts -> 4faibles
-
-	//Registres du Canal de son 4
-	BYTE NR41;//FF20 Durée de son (R/W)
-	BYTE NR42;//FF21 Enveloppe de volume (R/W)
-	BYTE NR43;//FF22 Compteur polynomial (R/W)
-	BYTE NR44;//FF23 Compteur/Consecutif; Initial (R/W)
-	
-	//Registres de contrôle du son 
-	BYTE NR50;//FF24 controle canal / on-off / volume (R/W)
-	BYTE NR51;//FF25 sortie son vers terminal droite / gauche
-	BYTE NR52;//FF26 son on-off
-
 	sc1_t channel1;//Canal de son 1
 	sc2_t channel2;//Canal de son 2
 	sc3_t channel3;//Canal de son 3
 	sc4_t channel4;//Canal de son 4
+	sound_control_t sound_controller;//Controleur de son
 
 } apu_t;
+
+void init();
+void sound_run();
+#endif
