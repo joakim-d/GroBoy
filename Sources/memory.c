6fb8261bfb91f4d0720569a550195e8fffe22fac
joemurder <joemurder@joemurder-PC.(none)>
2013-03-18 20:08:05 +0100

    Correction pour la compilation

--- Sources/memory.c
+++ Sources/memory.c
@@ -134,7 +134,7 @@ void memory_init(char *rom_path){
 
 
 
-	inline BYTE memory_read(unsigned short addr){
+	BYTE memory_read(unsigned short addr){
 		if(addr <= 0x3FFF) 		//demande de lecture dans la banque de rom 0 de la cartouche
 			return cartridge_rom_buffer[addr];
 		if(addr >= 0x4000 && addr <= 0x7FFF) 		//demande de lecture dans la banque de rom 1..n de la cartouche
@@ -151,7 +151,7 @@ static void alloc_ram_mem(size_t size){
 	cartridge_ram_buffer = (BYTE *) malloc(size);
 }
 
-inline void memory_write(unsigned short addr, BYTE data){
+void memory_write(unsigned short addr, BYTE data){
 	if((addr <= 0x7FFF) || (addr >= 0xA000 && addr <= 0xBFFF)){
 		if(cartridge_type >= 0x01 || cartridge_type <= 0x03){//MBC1
 			write_mbc1(addr, data);		
--- Sources/memory.h
+++ Sources/memory.h
@@ -20,6 +20,6 @@ BYTE *cartridge_ram_buffer;			//Le buffer de ram représentant la mémoire vive 
 BYTE *cartridge_rom_buffer;			//Le buffer qui contient toutes les données d'une rom (représentant toutes les banques alignées)
 BYTE internal_ram[32768];			//Mémoire interne de la GB (n'étant pas en rapport avec la cartouche)
 void memory_init();				//Méthode initialisant les buffers cartridge_rom_buffer et cartrigde_ram_buffer
-inline BYTE memory_read(unsigned short addr);	
-inline void memory_write(unsigned short addr, BYTE data);
+BYTE memory_read(unsigned short addr);	
+void memory_write(unsigned short addr, BYTE data);
 #endif
