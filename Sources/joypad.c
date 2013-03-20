e64ad009aa214edd3cfafca1620fb6bbdf1267af
burger <burger@buyonegetone.(none)>
2013-03-20 16:37:26 +0100

    Correction pad

--- Sources/joypad.c
+++ Sources/joypad.c
@@ -1,7 +1,7 @@
 #include "joypad.h"
 
 void joypad_update(int cycles){
-	BYTE joy_stat;
+	static BYTE joy_stat = 0xCF;
 	BYTE key_buttons; //
 	SDL_Event event;
 
@@ -10,46 +10,49 @@ void joypad_update(int cycles){
 
 	if(joypad_counter > 80000){
 		joy_stat = memory_read(0xFF00);
-		joy_stat |= 0xCF;
 		key_buttons = 0xFF;
 		while(SDL_PollEvent(&event)){;
 			switch(event.type)
 			{
 				case SDL_QUIT:
-					exit(0);
+					exit(0);break;
+				case SDL_KEYDOWN:
+					switch(event.key.keysym.sym){
+						case SDLK_RIGHT: 
+							key_buttons &= 0xFE;
+							break;
+						case SDLK_LEFT:
+							key_buttons &= 0xFD;
+							break;
+						case SDLK_DOWN:
+							key_buttons &= 0xFB;
+							break;
+						case SDLK_UP:
+							key_buttons &= 0xF7;
+							break;
+						case SDLK_d:
+							key_buttons &= 0xEF;
+							break;
+						case SDLK_s:
+							key_buttons &= 0xDF;
+							break;
+						case SDLK_RSHIFT:
+							key_buttons &= 0xBF;
+							break;
+						case SDLK_BACKSPACE:
+							key_buttons &= 0x7F;
+							break;
+						default: 
+							break;
+					}
 					break;
-			}
-			switch(event.key.keysym.sym){
-				case SDLK_RIGHT: 
-					key_buttons &= 0xFE;
-					break;
-				case SDLK_LEFT:
-					key_buttons &= 0xFD;
-					break;
-				case SDLK_DOWN:
-					key_buttons &= 0xFB;
-					break;
-				case SDLK_UP:
-					key_buttons &= 0xF7;
-					break;
-				case SDLK_d:
-					key_buttons &= 0xEF;
-					break;
-				case SDLK_s:
-					key_buttons &= 0xDF;
-					break;
-				case SDLK_RSHIFT:
-					key_buttons &= 0xBF;
-					break;
-				case SDLK_BACKSPACE:
-					key_buttons &= 0x7F;
-					break;
-				default: break;
 			}
 			joypad_counter %= 80000;
 		}
-		//printf("key_buttons\n");
-		if(key_buttons != 0xFF) make_request(JOYPAD);
+		if(key_buttons != 0xFF){
+			printf("TONBOU:%x\n", key_buttons);
+			make_request(JOYPAD);
+		}
 		if(!(0x20 & joy_stat)) joy_stat = (joy_stat & 0xF0) | (key_buttons & 0x0F);
 		if(!(0x10 & joy_stat)) joy_stat = (joy_stat & 0xF0) | ((key_buttons & 0xF0)>>4);
 		memory_write(0xFF00, joy_stat);
