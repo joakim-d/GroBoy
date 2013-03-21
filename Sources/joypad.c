8c11ef7f7f8edf8bd52dd62b7c51e650433cac1c
joemurder <joemurder@joemurder-PC.(none)>
2013-03-21 23:26:10 +0100

    Correction du joypad

--- Sources/joypad.c
+++ Sources/joypad.c
@@ -5,82 +5,42 @@ void joypad_update(int cycles){
 	static BYTE joy_new;
 	static BYTE joy_cur;
 	static BYTE joy_old = 0;
-	static BYTE key_buttons = 0xFF ; //
+	static BYTE key_buttons = 0xFF; //
+	static Uint8 *keystate;
 	SDL_Event event;
 
 	joypad_counter += cycles;
 
 	joy_cur = memory_read(0xFF00);
-
+	if(joypad_counter > 80000){
 		while(SDL_PollEvent(&event)){
 			switch(event.type){
 				case SDL_QUIT:
 					exit(0);
 					break;
-				case SDL_KEYDOWN:
-					switch(event.key.keysym.sym){
-						case SDLK_RIGHT: 
-							key_buttons &= 0xFE;
-							break;
-						case SDLK_LEFT:
-							key_buttons &= 0xFD;
-							break;
-						case SDLK_UP:
-							key_buttons &= 0xFB;
-							break;
-						case SDLK_DOWN:
-							key_buttons &= 0xF7;
-							break;
-						case SDLK_d:
-							key_buttons &= 0xEF;
-							break;
-						case SDLK_s:
-							key_buttons &= 0xDF;
-							break;
-						case SDLK_RSHIFT:
-							key_buttons &= 0xBF;
-							break;
-						case SDLK_RETURN:
-							key_buttons &= 0x7F;
-							break;
-						default:
-							break;
-					}
-					break;
-				case SDL_KEYUP:
-					switch(event.key.keysym.sym){
-						case SDLK_RIGHT: 
-							key_buttons |= 0x01;
-							break;
-						case SDLK_LEFT:
-							key_buttons |= 0x02;
-							break;
-						case SDLK_UP:
-							key_buttons |= 0x04;
-							break;
-						case SDLK_DOWN:
-							key_buttons |= 0x08;
-							break;
-						case SDLK_d:
-							key_buttons |= 0x10;
-							break;
-						case SDLK_s:
-							key_buttons |= 0x20;
-							break;
-						case SDLK_RSHIFT:
-							key_buttons |= 0x40;
-							break;
-						case SDLK_RETURN:
-							key_buttons |= 0x80;
-							break;
-						default:
-							break;
-					}
-					break;
 				default:
 					break;
 			}
+		joypad_counter -= 80000;
+		keystate = SDL_GetKeyState(NULL);
+		if(keystate[SDLK_RIGHT]) key_buttons &=0xFE;
+		else key_buttons |= 0x01;
+		if(keystate[SDLK_LEFT]) key_buttons &=0xFD;
+		else key_buttons |= 0x02;
+		if(keystate[SDLK_UP]) key_buttons &=0xFB;
+		else key_buttons |= 0x04;
+		if(keystate[SDLK_DOWN]) key_buttons &=0xF7;
+		else key_buttons |= 0x08;
+		if(keystate[SDLK_d]) key_buttons &=0xEF;
+		else key_buttons |= 0x10;
+		if(keystate[SDLK_s]) key_buttons &=0xDF;
+		else key_buttons |= 0x20;
+		if(keystate[SDLK_RSHIFT]) key_buttons &=0xBF;
+		else key_buttons |= 0x40;
+		if(keystate[SDLK_RETURN]) key_buttons &=0x7F;
+		else key_buttons |= 0x80;
 		}
+	}
 
 	joy_new = 0x0F;
 	if(0x10 & joy_cur) {
