36544da24532603be23f501861b21a9e84b16d58
joemurder <joemurder@joemurder-PC.(none)>
2013-03-21 11:09:39 +0100

    Debuggage du joystick

--- Sources/joypad.c
+++ Sources/joypad.c
@@ -5,12 +5,11 @@ void joypad_update(int cycles){
 	static BYTE joy_new;
 	static BYTE joy_cur;
 	static BYTE joy_old = 0;
-	BYTE key_buttons; //
+	static BYTE key_buttons = 0xFF ; //
 	SDL_Event event;
 
 	joypad_counter += cycles;
 
-	key_buttons = 0xFF;
 	joy_cur = memory_read(0xFF00);
 
 	if(joypad_counter >= 80000){
@@ -43,8 +42,38 @@ void joypad_update(int cycles){
 						case SDLK_RSHIFT:
 							key_buttons &= 0xBF;
 							break;
-						case SDLK_BACKSPACE:
+						case SDLK_RETURN:
 							key_buttons &= 0x7F;
+							break;
+						default:
+							break;
+					}
+					break;
+				case SDL_KEYUP:
+					switch(event.key.keysym.sym){
+						case SDLK_RIGHT: 
+							key_buttons |= 0x01;
+							break;
+						case SDLK_LEFT:
+							key_buttons |= 0x02;
+							break;
+						case SDLK_DOWN:
+							key_buttons |= 0x04;
+							break;
+						case SDLK_UP:
+							key_buttons |= 0x08;
+							break;
+						case SDLK_d:
+							key_buttons |= 0x10;
+							break;
+						case SDLK_s:
+							key_buttons |= 0x20;
+							break;
+						case SDLK_RSHIFT:
+							key_buttons |= 0x40;
+							break;
+						case SDLK_RETURN:
+							key_buttons |= 0x80;
 							break;
 						default:
 							break;
@@ -59,17 +88,17 @@ void joypad_update(int cycles){
 	//printf("%x\n",(joy_stat & 0xF0) | (key_buttons & 0x0F));
 	//printf("%x\n",(joy_stat & 0xF0) | ((key_buttons & 0xF0)>>4));
 	joy_new = 0x0F;
-	if(0x20 & joy_cur) {
+	if(0x10 & joy_cur) {
 		joy_new &= ((key_buttons & 0xF0)>>4);
 	}
-	if(0x10 & joy_cur) {
+	if(0x20 & joy_cur) {
 		joy_new &= (key_buttons & 0x0F);
 	}
-
 
 	memory_write(0xFF00, (joy_cur & 0xF0) | joy_new);
 	if(joy_new != joy_old){
 		joy_old = joy_new;
+		make_request(JOYPAD);
 	}
 }
 
