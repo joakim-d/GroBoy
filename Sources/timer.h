0168ffbfdde19b653d0511fab770f13eaf1d6b79
joemurder <joemurder@joemurder-PC.(none)>
2013-03-31 18:33:57 +0200

    Correction du timer

--- Sources/timer.h
+++ Sources/timer.h
@@ -5,6 +5,6 @@
 #include "memory.h"
 #define CLOCK_SPEED 4194304
 #define DIVIDER_TIMER_SPEED 256
-static const int tac_speed[] = {4096, 262144, 65536, 16384};
+static const int tac_speed[] = {1024, 16, 64, 256};
 void timer_update(BYTE cycles);
 #endif
