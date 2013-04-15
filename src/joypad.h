1b9ed5497f4f258fa85e0e5323195aeb62bdf2cb
joemurder <joemurder@joemurder-PC.(none)>
2013-04-15 16:25:30 +0200

    Ebauche de la conf du joy

--- src/joypad.h
+++ src/joypad.h
@@ -5,7 +5,8 @@
 	#include "interrupts.h"
 	void joypad_update(int cycles);
 	void joypad_init();
+	SDL_Joystick *joystick;
 	enum {right, left, up, down, b, a, select_, start};
-	int joypad_config[8];
-
+	int keyboard_config[8];
+	int joystick_config[8];
 #endif
